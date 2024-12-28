6209497a8a6abbd804599cc9c600e35fcc8e97f2
rodrigo <rodrigodsd@gmail.com>
2024-12-27 22:16:33 -0300

    Add Exception Handler

--- src/main/java/com/frederico/investiments/user/ControllerAdvice.java
+++ src/main/java/com/frederico/investiments/user/ControllerAdvice.java
@@ -1,5 +1,10 @@
 package com.frederico.investiments.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
@@ -9,4 +14,11 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
     // TODO treat IllegalArgumentException
 
     // TODO treat UsernameNotFoundException
+
+    // TODO treat MaxUploadSizeExceededException
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ProblemDetail> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(ProblemDetail.forStatusAndDetail(HttpStatus.EXPECTATION_FAILED,"File too large."));
+    }
 }
