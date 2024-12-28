b4afc7689be6911c8e94675578c6e13d0aa2b1da
rodrigo <rodrigodsd@gmail.com>
2024-12-27 22:17:18 -0300

    Add Portfolio domain classes

---
+++ src/main/java/com/frederico/investiments/portfolio/PortfolioServiceImpl.java
@@ -0,0 +1,22 @@
+package com.frederico.investiments.portfolio;
+
+import com.frederico.investiments.portfolio.domain.Portfolio;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class PortfolioServiceImpl implements PortfolioService {
+
+    private PortfolioRepository portfolioRepository;
+
+    public PortfolioServiceImpl(PortfolioRepository portfolioRepository) {
+        this.portfolioRepository = portfolioRepository;
+    }
+
+    @Override
+    @Transactional
+    public Portfolio save(Portfolio portfolio) {
+        return portfolioRepository.save(portfolio);
+    }
+}
